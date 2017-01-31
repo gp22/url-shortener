e34fbb26b96a6c084fce7ad87b99ce033fdff35a
gp22 <gp22@tutanota.com>
2017-01-31 06:39:19 +0000

    work on implementing counter db

--- app.js
+++ app.js
@@ -14,28 +14,21 @@ mongoose.connect(uri);
 app.set('view engine', 'ejs');
 
 // to be removed VERY SOON!
-var urls = {};
-var shortUrls = {};
-var count = 1;
+// var urls = {};
+// var shortUrls = {};
+// var count = 1;
 
 var shortUrlSchema = new mongoose.Schema({
-    original_url: String,
-    short_url: Number
+    _id: Number,
+    original_url: String
 });
 
-var ShortUrl = mongoose.model('ShortUrl', shortUrlSchema);
+var counterSchema = new mongoose.Schema({
+    sequenceValue: Number
+});
 
-// ShortUrl.create({
-//     original_url: 'https://www.github.com',
-//     short_url: 3
-// }, function(err, shortUrl) {
-//     if(err) {
-//         console.error('ERROR');
-//     } else {
-//         console.log('Short URL created');
-//         console.log(shortUrl);
-//     }
-// });
+var Counter = mongoose.model('Counter', counterSchema);
+var ShortUrl = mongoose.model('ShortUrl', shortUrlSchema);
 
 app.get('/', function(req, res) {
     res.render('home');
@@ -56,7 +49,8 @@ app.get('/new/*', function(req, res) {
         response["short_url"] = shortUrl;
         res.send(response);
     } else {
-        response["error"] = 'Invalid URL: Make sure your URL follows the format http(s)://www.example.com';
+        response["error"] =
+        'Invalid URL: Use the format http(s)://www.example.com';
         res.send(response);
     }
 });
@@ -86,8 +80,8 @@ function getShortUrl(url) {
     gets number value of url used for creating shortened url
     adds key value pairs for urls/number values to db
 
-    arguments - valid url (string)
-    returns - number parameter for short url (string)
+    arguments: valid url (string)
+    returns: number parameter for short url (string)
     */
 
     // if (urls.hasOwnProperty(url)) {
@@ -100,7 +94,7 @@ function getShortUrl(url) {
     //     return urls[url];
     // }
 
-    ShortUrl.findOne({original_url: url}, function(err, shorturl) {
+    ShortUrl.findOne({ original_url: url }, function(err, shorturl) {
         if (err) {
             console.error(err);
         } else if (shorturl !== null) {
@@ -110,3 +104,54 @@ function getShortUrl(url) {
         }
     });
 };
+
+function getNextCount() {
+    /*
+    arguments: none
+    returns: next Counter value
+    */
+    // Counter.findOne({}, function(err, nextCount) {
+    //     if (err) {
+    //         console.error(err);
+    //     } else {
+    //         // increment the _id of Counter
+    //         // return _id of Counter
+    //         Counter.update({ $inc: { sequenceValue: 1 } }, function(err, result) {
+    //             if (err) {
+    //                 console.error(err);
+    //             } else {
+    //                 // console.log('Counter updated successfully');
+    //                 // console.log(result);
+    //             }
+    //         });
+    //         console.log('next counter value');
+    //         console.log(nextCount.sequenceValue);
+    //     }
+    // });
+    Counter.findOneAndUpdate({}, { $inc: { sequenceValue: 1 } });
+};
+
+getNextCount();
+
+// ShortUrl.create({
+//     _id: 1,
+//     original_url: 'https://www.google.com'
+// }, function(err, shortUrl) {
+//     if(err) {
+//         console.error('ERROR');
+//     } else {
+//         console.log('Short URL created');
+//         console.log(shortUrl);
+//     }
+// });
+
+// Counter.create({
+//     _id: 2
+// }, function(err, shortUrl) {
+//     if(err) {
+//         console.error('ERROR');
+//     } else {
+//         console.log('Counter created');
+//         console.log(shortUrl);
+//     }
+// });
