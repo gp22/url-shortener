08fedb69ea1f4b3f5e4790bc03a1720b695109d4
gp22 <gp22@tutanota.com>
2017-02-07 09:29:58 +0000

    update README.md and comments in app.js

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 URL Shortener
 
-When you pass a URL as a parameter you will receive a shortened URL in JSON
+The URL shortener microservice performs a very simple function when it is passed a URL (example: `https://www.google.com`) as a parameter:
+
+It will generate a shortened version of the URL in JSON format (example: `https://gp22-shorturl.herokuapp.com/1000`).
+
+When you enter the short version of the URL into your web browsers address bar it will redirect you to your original URL.
+
+If you enter either an invalid URL or an invalid short URL it will respond with an error in JSON format.
+
+One thing about this app that I think is really funny is that the short URLs it generates for you are often longer than the actual URL! But it was a fun app to build either way!
+
+Example to create a new short URL
+
+`https://gp22-shorturl.herokuapp.com/new/https://www.google.com`
+
+Example response
+
+`{ "original_url": "https://www.google.com", "short_url": "https://gp22-shorturl.herokuapp.com/1000" }`
+
+Example usage
+
+`https://gp22-shorturl.herokuapp.com/1000`
+
+Redirects to
+
+`https://www.google.com`
--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+/*
+URL Shortener
+The URL shortener microservice performs a very simple function when it is
+passed a URL (example: `https://www.google.com`) as a parameter:
+It will generate a shortened version of the URL in JSON format
+(example: `https://gp22-shorturl.herokuapp.com/1000`).
+When you enter the short version of the URL into your web browsers address
+bar it will redirect you to your original URL.
+If you enter either an invalid URL or an invalid short URL it will respond
+with an error in JSON format.
+One thing about this app that I think is really funny is that the short URLs
+it generates for you are often longer than the actual URL! But it was a fun
+app to build either way!
+Example to create a new short URL
+https://gp22-shorturl.herokuapp.com/new/https://www.google.com
+Example response
+{ "original_url": "https://www.google.com",
+"short_url": "https://gp22-shorturl.herokuapp.com/1000" }
+Example usage
+https://gp22-shorturl.herokuapp.com/1000
+Redirects to
+https://www.google.com
+*/
+'use strict';
+
 var express = require('express');
 var app = express();
 var validUrl = require('valid-url');
@@ -16,7 +41,7 @@ const uri = process.env.MLAB_URI;
 // Comment it out when using the following const values for local testing
 const APPURL = 'https://gp22-shorturl.herokuapp.com/';
 
-// Use these const values for local testing
+// Use these values for local testing
 // const HOSTNAME = 'localhost';
 // const PORT = 3000;
 // const DBNAME = 'shorturls';
